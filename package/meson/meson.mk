646bcca1b0e784a964b914f3370fd1940d54142b
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-11-18 23:16:48 +0100

    meson: new package
    This new package provides the host variant of the Meson Build System, an
    open source build system meant to be both extremely fast, and as user
    friendly as possible.
    
    More precisely, Meson creates configuration files for the Ninja build
    system.
    
    Besides building Meson, it generates a cross-compilation configuration
    file ("$(HOST_DIR)/etc/meson/cross-compilation.conf").

---
+++ package/meson/meson.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# meson
+#
+################################################################################
+
+MESON_VERSION = 0.36.0
+MESON_SITE = $(call github,mesonbuild,meson,$(MESON_VERSION))
+MESON_LICENSE = Apache-2.0
+MESON_LICENSE_FILES = COPYING
+MESON_SETUP_TYPE = setuptools
+
+HOST_MESON_DEPENDENCIES = host-ninja
+HOST_MESON_NEEDS_HOST_PYTHON = python3
+
+HOST_MESON_TARGET_ENDIAN = $(shell echo $(BR2_ENDIAN) | tr 'A-Z' 'a-z')
+
+define HOST_MESON_INSTALL_CROSS_CONF
+	$(INSTALL) -d $(HOST_DIR)/etc/meson
+	sed -e 's;@TARGET_CROSS@;$(TARGET_CROSS);g' \
+		-e 's;@TARGET_ARCH@;$(ARCH);g' \
+		-e 's;@TARGET_ENDIAN@;$(HOST_MESON_TARGET_ENDIAN);g' \
+		-e 's;@HOST_DIR@;$(HOST_DIR);g' \
+		$(BR2_EXTERNAL_ELEBIHAN_PATH)/package/meson/cross-compilation.conf.in > \
+		$(HOST_DIR)/etc/meson/cross-compilation.conf
+endef
+
+HOST_MESON_POST_INSTALL_HOOKS += HOST_MESON_INSTALL_CROSS_CONF
+
+$(eval $(host-python-package))
