cfa346768164d093131eb358c4b58420599422ad
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-12-14 20:09:18 +0100

    wolfssl: new package
    This new package provides wolfSSL (formerly CyaSSL), a small, fast,
    portable implementation of TLS/SSL for embedded devices to the cloud.

---
+++ package/wolfssl/wolfssl.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# wolfssl
+#
+################################################################################
+
+WOLFSSL_VERSION = 3.9.10
+WOLFSSL_SITE = $(call github,wolfSSL,wolfssl,v$(WOLFSSL_VERSION)-stable)
+WOLFSSL_LICENSE = GPLv2 or COMMERCIAL
+WOLFSSL_LICENSE_FILES = COPYING
+WOLFSSL_INSTALL_STAGING = YES
+WOLFSSL_AUTORECONF = YES
+
+WOLFSSL_CONF_OPTS = \
+	--disable-jobserver \
+	--disable-examples \
+	--disable-crypttests
+
+ifeq ($(BR2_arm)$(BR2_aarch64),y)
+WOLFSSL_CONF_OPTS += --enable-armasm
+endif
+
+define WOLFSSL_REMOVE_CONFIG_TOOL_HOOK
+	rm $(TARGET_DIR)/usr/bin/wolfssl-config
+endef
+
+WOLFSSL_POST_INSTALL_TARGET_HOOKS += WOLFSSL_REMOVE_CONFIG_TOOL_HOOK
+
+$(eval $(autotools-package))
