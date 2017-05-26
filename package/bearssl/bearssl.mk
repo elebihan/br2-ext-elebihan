f1209417ac077e24c954be1b7b7ddc97d87d539a
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2017-05-26 21:52:45 +0200

    bearssl: bump version to 0.4

--- package/bearssl/bearssl.mk
+++ package/bearssl/bearssl.mk
@@ -4,18 +4,27 @@
 #
 ################################################################################
 
-BEARSSL_VERSION = 0.1
+BEARSSL_VERSION = 0.4
 BEARSSL_SITE = https://www.bearssl.org
 BEARSSL_LICENSE = MIT
 BEARSSL_LICENSE_FILES = LICENSE.txt
 BEARSSL_INSTALL_STAGING = YES
 
-BEARSSL_MAKE_OPTS = \
-	CC="$(TARGET_CC)"
+BEARSSL_MAKE_OPTS = CONF=Buildroot
+
+define BEARSSL_CONFIGURE_CMDS
+	echo "include conf/Unix.mk" > $(@D)/conf/Buildroot.mk
+	echo "CC=$(TARGET_CC)" >> $(@D)/conf/Buildroot.mk
+	echo "LD=$(TARGET_CC)" >> $(@D)/conf/Buildroot.mk
+	echo "LDDLL=$(TARGET_CC)" >> $(@D)/conf/Buildroot.mk
+	echo "AR=$(TARGET_AR)" >> $(@D)/conf/Buildroot.mk
+	echo "CFLAGS=$(TARGET_CFLAGS)" >> $(@D)/conf/Buildroot.mk
+	echo "LDFLAGS=$(TARGET_LDFLAGS)" >> $(@D)/conf/Buildroot.mk
+endef
 
 ifeq ($(BR2_PACKAGE_BEARSSL_BIN),y)
 define BEARSSL_INSTALL_BIN
-	$(INSTALL) -D -m 0755 $(@D)/brssl $(TARGET_DIR)/usr/bin/brssl
+	$(INSTALL) -D -m 0755 $(@D)/build/brssl $(TARGET_DIR)/usr/bin/brssl
 endef
 endif
 
@@ -24,7 +33,7 @@ define BEARSSL_BUILD_CMDS
 endef
 
 define BEARSSL_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/libbearssl.a \
+	$(INSTALL) -D -m 0644 $(@D)/build/libbearssl.a \
 		$(STAGING_DIR)/usr/lib/libbearssl.a
 	$(INSTALL) -d $(STAGING_DIR)/usr/include/bearssl
 	$(INSTALL) -m 0644 $(@D)/inc/*.h $(STAGING_DIR)/usr/include/bearssl
